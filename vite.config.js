17b150f53ce979323bde76b85372b76f76ee32f4
Ervin Gorospe <ervingorospe.code@gmail.com>
2025-04-07 22:10:30 +0800

    updated base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/build/',
+    base: process.env.APP_URL ? `${process.env.APP_URL}/build/` : '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],

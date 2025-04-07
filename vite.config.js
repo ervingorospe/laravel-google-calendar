6c779ab9d39fd6c42d3783810f484ab1b9018e2a
Ervin Gorospe <ervingorospe.code@gmail.com>
2025-04-07 21:52:10 +0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -10,4 +11,11 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        https: true, // Ensures the Vite server uses HTTPS in production
+    },
+    build: {
+        // Force Vite to generate assets using HTTPS
+        assetsPublicPath: process.env.APP_URL + '/build/',
+    },
 });
